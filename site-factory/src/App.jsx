9b2612d2439fd994ee86ec750a487e8d605a0c84
Hnss <loozsky@gmail.com>
2025-03-21 23:27:53 +0800

    添加 Tailwind CSS 和 PostCSS 配置，重构样式和工具组件

--- site-factory/src/App.jsx
+++ site-factory/src/App.jsx
@@ -1,30 +1,41 @@
-import "./App.css";
 import { tools } from "./lib/tools";
 
 export default function App() {
   return (
     <>
-      <main className="container">
-        <section className="welcome">
-          See All of My Favorite{" { "}
+      <div className="w-screen h-screen flex flex-col justify-center items-center gap-[2rem]">
+        <section className="text-4xl font-bold">
+          {" { "}
           <b>
             <a href="https://site.luling.xyz">Tools</a>
           </b>
           {" } "}
         </section>
-        <section className="tools">
+        <section className="flex flex-col gap-[1rem]">
           {tools.map((tool) => (
-            <div className="tool" key={tool.toolName}>
-              <a href={tool.toolUrl} target="_blank" title={tool.description}>
-                {tool.toolName}
-              </a>
-              <span className="updated-date">
-                {" (Updated: " + tool.updatedDate + ")"}
-              </span>
-            </div>
+            <a
+              href={tool.toolUrl}
+              target="_blank"
+              title={tool.updatedDate}
+              key={tool.toolName}
+            >
+              <div className="rounded-lg border text-card-foreground relative overflow-hidden transition-colors bg-zinc-50  border-zinc-200  hover:border-zinc-500">
+                <div className="flex items-center p-4">
+                  <div className="flex-shrink-0 mr-4">{tool.emoji}</div>
+                  <div className="flex-grow min-w-[376px]">
+                    <h2 className="text-lg font-medium truncate text-zinc-900 dark:text-zinc-100">
+                      {tool.toolName}
+                    </h2>
+                    <span className="text-xs truncate text-zinc-900 dark:text-zinc-100 opacity-80">
+                      {tool.description}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </a>
           ))}
         </section>
-      </main>
+      </div>
     </>
   );
 }
